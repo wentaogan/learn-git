07e216a2fb84cc7f8a0dc51297dc3d7cf28a0f02
wentao <ganwentao03@gmail.com>
2023-11-04 01:34:24 +0800

    Add introduction

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,10 @@
 console.log('Start');
+console.log("introduction");
+let j = 0;
+while(j < 10){
+   console.log(`Introducing ${j}`);
+   j++;
+}
 let i = 0;
  while(i < 10){
     console.log(`Hello World${i + 1}`);
