80b6f1fa86c517f4b7661128dc7125210de83025
wentao <ganwentao03@gmail.com>
2023-11-04 01:31:55 +0800

    Display credits in the end

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,10 @@ console.log('Start');
 let i = 0;
  while(i < 10){
     console.log(`Hello World${i + 1}`);
-    console.log('==============')
+    console.log('------------')
  }
-console.log('The end!')
+console.log('The end!');
+console.log('Credits:');
+for(let i = 0; i < 10; i++){
+   console.log(`Name ${i}`);
+}
